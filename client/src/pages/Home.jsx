68c2e976675657171b60101137c6de0122f6bda4
akarshan gupta <akarshang31@gmail.com>
2026-01-20 15:45:58 +0530

    created footer

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import HeroSection from '../components/HeroSection'
 import FeaturedSection from '../components/FeaturedSection'
 import MovieCard from '../components/MovieCard'
 import TrailerSection from '../components/TrailerSection'
+import Footer from '../components/Footer'
 
 const Home = () => {
   return (
